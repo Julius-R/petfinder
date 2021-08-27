f65ce24f7771df3142478bc2155ceb55486a78bc
David-Lyu <lyu.david@yahoo.com>
2021-08-26 20:02:56 -0700

    Julius sucks

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
 
 export default function Home() {
-	return (
-		<Layout title="Pet Finders | Find Your Missing Companion Here">
-			Test
-		</Layout>
-	);
+  return (
+    <Layout title="Pet Finders | Find Your Missing Companion Here">
+      Test Julius sucks
+    </Layout>
+  );
 }
